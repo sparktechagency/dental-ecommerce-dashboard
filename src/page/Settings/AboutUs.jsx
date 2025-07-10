5883f92536236b092c2570f2476be7f7b42191ce
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-07-10 23:04:04 +0600

    settings page

--- src/page/Settings/AboutUs.jsx
+++ src/page/Settings/AboutUs.jsx
@@ -1,9 +1,35 @@
-const AboutUs = () => {
-    return (
-        <div>
-            <h1>About Us</h1>
-        </div>
-    );
-};
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import PageHeading from "../../shared/PageHeading";
+
+function AboutUs() {
+  const [content, setContent] = useState(
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum."
+  );
+
+  return (
+    <div className="p-5">
+      <PageHeading title="About Us" />
+
+      <div className=" bg-white rounded shadow p-5 h-full mt-5">
+        <ReactQuill
+          style={{ padding: "10px" }}
+          theme="snow"
+          value={content}
+          onChange={setContent}
+        />
+      </div>
+      <div className="text-center py-5">
+        <button
+          onClick={() => console.log(content)}
+          className="bg-[#136BFB] text-[#fff] font-semibold w-full py-2 rounded transition duration-200"
+        >
+          Save changes
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default AboutUs;
--- src/page/Settings/TermsCondition.jsx
+++ src/page/Settings/TermsCondition.jsx
@@ -23,7 +23,7 @@ function TermsAndCondition() {
       <div className="text-center py-5">
         <button
           onClick={() => console.log(content)}
-          className="bg-[#91DF92] text-[#000] font-semibold w-full py-2 rounded transition duration-200"
+          className="bg-[#136BFB] text-[#fff] font-semibold w-full py-2 rounded transition duration-200"
         >
           Save changes
         </button>
